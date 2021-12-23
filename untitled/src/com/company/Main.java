1dfdfaf068bba44ea1f4d8dfe3793ec1bc71eff4
plnppt <tockareva@list.ru>
2021-12-23 11:33:02 +0300

    all been commit

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String s = "dad";
-	System.out.println(s);
+	    String s = "dad";
+	    System.out.println(s);
+	    System.out.println(12);
     }
 }
